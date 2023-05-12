4b57a12141a705cf60df70451e091161fc6647ea
sofi19962023 <sofi.espinoza2015@gmail.com>
2023-05-11 23:20:10 -0300

    porfolio-angular

--- porfolio-angular/src/app/app.module.ts
+++ porfolio-angular/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoArgProgramaComponent } from './components/logo-arg-programa/logo-arg-programa.component';
 import { RedessocialesComponent } from './components/redessociales/redessociales.component';
+import { BannerComponent } from './components/banner/banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoArgProgramaComponent,
-    RedessocialesComponent
+    RedessocialesComponent,
+    BannerComponent
   ],
   imports: [
     BrowserModule,
