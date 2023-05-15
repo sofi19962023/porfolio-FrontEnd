61b0118b19676dcb2e490dbadf52ecfc24cbce65
sofi19962023 <sofi.espinoza2015@gmail.com>
2023-05-15 13:15:19 -0300

    porfolio-backend

---
+++ porfolio-backend/src/main/java/com/porfolio/LorenaCabrera/LorenaCabreraApplication.java
@@ -0,0 +1,13 @@
+package com.porfolio.LorenaCabrera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LorenaCabreraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LorenaCabreraApplication.class, args);
+	}
+
+}
---
+++ porfolio-backend/src/test/java/com/porfolio/LorenaCabrera/LorenaCabreraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.porfolio.LorenaCabrera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LorenaCabreraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
